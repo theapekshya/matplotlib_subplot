777feee2430d0472772e9522a63271cea29b5ab8
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-12-29 19:50:28 +0545

    matplotlib_subplot

---
+++ subplot.py
@@ -0,0 +1,11 @@
+# matplotlib subplot
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
